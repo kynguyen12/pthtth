a7ef2f097de48faab2228e085c72440e9b7cbb12
kynguyen12 <56568079+kynguyen12@users.noreply.github.com>
2020-09-29 14:33:43 +0700

    tuan1

---
+++ bai1.java
@@ -0,0 +1,11 @@
+package ky;
+
+public class bai1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ bai2.java
@@ -0,0 +1,14 @@
+package ky;
+import java.util.Scanner; 
+
+public class bai2 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in); 
+        System.out.println("Ban ten gi ?"); 
+        String str = scanner.nextLine(); 
+        System.out.println("Hi minh ten vanbucu, xin chao "+str); 
+
+	}
+
+}
