c962c6020e919c18330dd0876dbe2e6376409304
kynguyen12 <56568079+kynguyen12@users.noreply.github.com>
2020-10-06 00:45:28 -0700

    lab 3

---
+++ bai3.java
@@ -0,0 +1,23 @@
+package kynguyen;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+public class bai3 {
+
+	public static void main(String[] args) {
+		try{
+			InetAddress host = InetAddress.getByName(args[0]);
+			String hostName = host.getHostName();
+			System.out.println("Host name:"+hostName);
+			System.out.println("Dia chi IP:"+host.getHostAddress());
+			}
+			catch(UnknownHostException e)
+			{
+			System.out.println("Khong tim thay dia chi");
+			return;
+			}
+		
+
+	}
+
+}
